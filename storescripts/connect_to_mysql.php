2ef5e1d3f56f28872d5abb1cdb793098caf39258
ArunNewBee <arundas2409@gmail.com>
2018-04-27 16:21:57 +0530

    Add files via upload

---
+++ storescripts/connect_to_mysql.php
@@ -0,0 +1,11 @@
+<?php
+ //$db_conx = mysqli_connect("50.62.209.15:3306", "habro", "storE4mycarT?~", "sadik2015_");
+$db_conx = mysqli_connect("localhost", "root", "", "habro");
+// Evaluate the connection
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit();
+} else {
+	
+}
+?>
